1069f3c64b1a35a498aaafb1b5151ccf3e2c54c0
Arya Tri Putra Majiah <2072023@maranatha.ac.id>
2022-09-09 16:27:21 +0700

    Setup Project
    - install chakra ui
    - intall eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const customTheme = extendTheme();
+
+function MyApp({Component, pageProps}) {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
