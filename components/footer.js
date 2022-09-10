3b389c42a4695c19a662ac46d7812e1f56d27089
sherly santiadi <sntdshrly@gmail.com>
2022-09-10 21:27:56 +0700

    update

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 export default function Footer() {
   return (
     <Box bg={useColorModeValue("gray.50", "gray.900")} color={useColorModeValue("gray.700", "gray.200")}>
-      <Container as={Stack} maxW={"6xl"} py={4} direction={{ base: "column", md: "row" }} spacing={4} justify={{ base: "center", md: "space-between" }} align={{ base: "center", md: "center" }}>
+      <Container as={Stack} maxW={"6xl"} py={2} direction={{ base: "column", md: "row" }} spacing={4} justify={{ base: "center", md: "space-between" }} align={{ base: "center", md: "center" }}>
         <Icon />
         <Text>© 2022 PT. DIGILAB INDONESIA. ALL RIGHTS RESERVED</Text>
         <Stack direction={"row"} spacing={2}>
--- components/portofolio.js
+++ components/portofolio.js
@@ -1,6 +1,6 @@
 import { Box, Center, useColorModeValue, Heading, Text, Stack, Image, useBreakpointValue } from "@chakra-ui/react";
 
-const IMAGE = "https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80";
+const IMAGE = "https://goodmockups.com/wp-content/uploads/2020/05/Free-Product-Software-Box-Packaging-Mockup-1.jpg";
 
 export default function Portofolio() {
   return (
@@ -38,17 +38,17 @@ export default function Portofolio() {
           </Box>
           <Stack pt={10} align={"center"}>
             <Text color={"gray.500"} fontSize={"sm"} textTransform={"uppercase"}>
-              Brand
+              Digilab
             </Text>
-            <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-              Application Name
+            <Heading fontSize={"2l"} fontFamily={"body"} fontWeight={500}>
+              Digilab Enterprise Resource Planning
             </Heading>
             <Stack direction={"row"} align={"center"}>
               <Text fontWeight={800} fontSize={"xl"}>
-                Android
+                Linux
               </Text>
               <Text textDecoration={"line-through"} color={"gray.600"}>
-                IOS
+                Windows
               </Text>
             </Stack>
           </Stack>
