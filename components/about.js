b5393435938a4f3f8d1a2661dc5433b78dac5d57
sherly santiadi <sntdshrly@gmail.com>
2022-09-25 18:28:13 +0700

    Update about.js

--- components/about.js
+++ components/about.js
@@ -89,7 +89,7 @@ export default function About() {
             top={"50%"}
             transform={"translateX(-50%) translateY(-50%)"}
           />
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/Z9QbYZh1YXY?start=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/J6-M7onlYoI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Box>
       </Flex>
       </Stack>
